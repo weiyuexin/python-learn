45f337604358dbdb0268dc87bc69ce64f5cdf15d
weiyuexin <3022422894@qq.com>
2021-03-17 23:11:18 +0800

    first

---
+++ Games/1-100数字猜谜游戏.py
@@ -0,0 +1,16 @@
+import random
+print("1-100数字猜谜游戏")
+num = random.randint(1, 100)
+guess = "guess"
+i = 0
+while guess != num:
+    i += 1
+    guess = int(input("请输入你猜的数字:"))
+
+    if guess == num:
+        print("恭喜，你猜对了！")
+    elif guess > num:
+        print("你猜的数大了�?")
+    else:
+        print("你猜的数小了�?")
+print("你一共猜�?%d" % i + "次，快和你的朋友比试一下吧！！�?")
